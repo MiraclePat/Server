5412c99ee7d2100d333cea2b5883e59b76470d3d
Kogu99 <kohyeon17@gmail.com>
2023-12-23 01:31:27 +0900

    CI/CD 적용을 위한 설정 수정

--- src/main/java/com/miraclepat/config/FirebaseConfig.java
+++ src/main/java/com/miraclepat/config/FirebaseConfig.java
@@ -4,13 +4,14 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.miraclepat.utils.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 @Slf4j
@@ -21,8 +22,8 @@ public class FirebaseConfig {
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         //발급받은 firebase key 를 등록해 계정 설정
-        FileInputStream serviceAccount =
-                new FileInputStream(Constants.PROJECT_FIREBASE_KEY);
+        Resource resource = new ClassPathResource("miracle-pat-firebase-adminsdk.json");
+        InputStream serviceAccount = resource.getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
